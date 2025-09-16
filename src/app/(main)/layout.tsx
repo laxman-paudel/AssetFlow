5a971ac21c33fd2290d939c235fe43581ddafc59
Lakshman Paudel <paudellaxman4@gmail.com>
2025-09-16 20:08:12 +0000

    remove transitions

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -40,7 +40,7 @@ export default function MainLayout({
           </Button>
         </div>
       </header>
-      <main key={pathname} className="flex-1 pb-24 md:pb-8 animate-fade-in">
+      <main className="flex-1 pb-24 md:pb-8">
         {children}
       </main>
       <BottomNav />
